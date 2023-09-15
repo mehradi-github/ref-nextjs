1ec4a8508aa811647a05e88058533d6ec20c5a95
Mehradi <mehradi.info@gmail.com>
2023-09-15 08:59:06 +0200

    Adding Navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,10 +15,26 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const links = [
+    { href: '/', label: 'Home' },
+    { href: '/docs', label: 'Docs' },
+    { href: '/todos', label: 'Todo' },
+  ];
   return (
     <html lang="en">
       <body className={inter.className}>
-        <h1>Layout</h1>
+        <header>
+          <nav className="felx items-center">
+            {links.map((link) => (
+              <ul>
+                <li key={link.href}>
+                  <Link href={link.href}>{link.label}</Link>
+                </li>
+              </ul>
+            ))}
+          </nav>
+        </header>
+
         <div>{children}</div>
       </body>
     </html>

39a412da168a9b7da74c9a0ab0f4243d1715b2ac
Mehradi <mehradi.info@gmail.com>
2023-09-15 09:33:33 +0200

    Adding multiple class

--- app/(dashboard)/docs/[[...id]]/page.tsx
+++ app/(dashboard)/docs/[[...id]]/page.tsx
@@ -2,6 +2,6 @@ import styles from './style.module.css';
 
 const DocsSubPage = ({ params }) => {
   console.log(params);
-  return <div className={styles.title}>Docs Page</div>;
+  return <div className={`${styles.title} ${styles.another}`}>Docs Page</div>;
 };
 export default DocsSubPage;

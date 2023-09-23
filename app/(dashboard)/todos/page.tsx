6194d6d340b09f4fa607b8bf23a4b301a3f5a418
Mehradi <mehradi.info@gmail.com>
2023-09-22 19:08:55 -0700

    Get Data from db

--- app/(dashboard)/todos/page.tsx
+++ app/(dashboard)/todos/page.tsx
@@ -1,4 +1,11 @@
-const TodosPage = () => {
+import db from '@/utils/db';
+
+const getData = async () => {
+  const todos = await db.todo.findMany({});
+  return todos;
+};
+const TodosPage = async () => {
+  const todos = await getData();
   return <div>TodosPage</div>;
 };
 export default TodosPage;
